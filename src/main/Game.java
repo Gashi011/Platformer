815cfd9b4d3aa439b42c75a3a2775ed808f601df
Gashi011 <igorgasi@gmail.com>
2023-08-15 23:24:05 +0200

    Rectangle before starting the game

---
+++ src/main/Game.java
@@ -0,0 +1,44 @@
+package main;
+
+public class Game implements Runnable{
+    private GameWindow gW;
+    private GamePanel gP;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
+    public Game() {
+        gP = new GamePanel();
+        gW = new GameWindow(gP);
+        gP.requestFocus();
+        startGameLoop();
+    }
+
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+        while(true){
+            now = System.nanoTime();
+            if(now - lastFrame >= timePerFrame){
+                gP.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+
+            if(System.currentTimeMillis() - lastCheck >= 1000){
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+        }
+    }
+}
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,92 @@
+package main;
+
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private float xDelta = 100;
+    private float yDelta = 100;
+    private float xDir = 1f, yDir = 1f;
+    private int frames = 0;
+    private long lastCheck = 0;
+    private Random random;
+    private Color color = new Color(150, 20, 90);
+
+    public GamePanel() {
+        random = new Random();
+        mouseInputs = new MouseInputs(this);
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+    }
+
+    public void changeXDelta(int value){
+        this.xDelta += value;
+    }
+
+    public void changeYDelta(int value){
+        this.yDelta += value;
+    }
+
+    public void setRectPos(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+    }
+
+
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+
+
+
+        updateRectangle();
+        g.setColor(color);
+
+        g.fillRect((int) xDelta,(int) yDelta, 200, 50);
+
+
+        }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if(xDelta > 400 || xDelta< 0){
+            xDir *= -1;
+            color = getRandomColor();
+        }
+        yDelta +=yDir;
+        if(yDelta > 400 || yDelta < 0){
+            yDir *= -1;
+            color = getRandomColor();
+        }
+    }
+
+    private Color getRandomColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r, g, b);
+    }
+
+
+
+}
